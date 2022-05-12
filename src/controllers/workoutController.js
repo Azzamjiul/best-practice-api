f501471c6ef263e6e6635f4de0031f6a43d398f4
Azzam Jihad Ulhaq <azzam.jihad@moladin.com>
2022-05-13 05:09:38 +0700

    create workout and get one workout

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,64 @@
+const workoutService = require('../services/workoutService');
+
 const getAllWorkouts = (req, res) => {
-    res.send("Get all worouts");
+    const allWorkouts = workoutService.getAllWorkouts();
+    res.status(200).send({
+        status: "OK",
+        data: allWorkouts,
+    });
 };
 
 const getOneWorkout = (req, res) => {
-    res.send("Get an existing workout");
+    const {
+        params: { workoutId },
+    } = req;
+    
+    if (!workoutId) {
+        res.status(400).send("workoutId param required");
+        return;
+    }
+
+    const workout = workoutService.getOneWorkout(workoutId);
+    res.status(200).send({
+        status: "OK",
+        data: workout,
+    });
 };
 
 const createNewWorkout = (req, res) => {
-    res.send("Create a new workout");
+    const { body } = req;
+    if (
+        !body.name ||
+        !body.mode ||
+        !body.equipment ||
+        !body.exercises ||
+        !body.trainerTips
+    ) {
+        return;
+    }
+
+    const newWorkout = {
+        name: body.name,
+        mode: body.mode,
+        equipment: body.equipment,
+        exercises: body.exercises,
+        trainerTips: body.trainerTips,
+    }
+
+    const createdWorkout = workoutService.createNewWorkout(newWorkout);
+    res.status(201).send({
+        status: "OK",
+        data: createdWorkout,
+    });
 };
 
 const updateOneWorkout = (req, res) => {
+    const updatedWorkout = workoutService.updateOneWorkout();
     res.send("Update an existing workout");
 };
 
 const deleteOneWorkout = (req, res) => {
+    workoutService.deleteOneWorkout();
     res.send("Delete an existing workout");
 };
 
